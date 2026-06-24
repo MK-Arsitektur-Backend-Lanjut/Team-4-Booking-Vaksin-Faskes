3beb02a27159994d978eab61846320df5d4ce3f4
Laakdal <ario.rafa38@gmail.com>
2026-06-25 01:53:11 +0700

    Make the root redirect route cacheable for route:cache

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\DocsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('docs.index');
-});
+// Route::redirect (not a closure) so `php artisan route:cache` can serialize it.
+Route::redirect('/', '/docs');
 
 Route::get('/docs', [DocsController::class, 'index'])->name('docs.index');
 Route::get('/docs/openapi.yaml', [DocsController::class, 'openapi'])->name('docs.openapi');
